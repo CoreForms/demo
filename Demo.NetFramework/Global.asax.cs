9bf6c0157e6deff5a4a4231059a7d06a1a7ce31c
Patrick Widauer <patrick.widauer@rubicon.eu>
2022-09-01 11:56:28 +0200

    Add .NET Framework reference project

---
+++ Demo.NetFramework/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace Demo.NetFramework
+{
+  public class Global : HttpApplication
+  {
+    void Application_Start(object sender, EventArgs e)
+    {
+    }
+  }
+}
